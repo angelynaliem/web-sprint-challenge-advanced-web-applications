82c351d5f23c57fc397d25f83fbe0eead3e22631
angelynaliem <angelynaliem@gmail.com>
2020-10-12 02:39:09 -0400

    Updated testing

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -13,8 +13,7 @@ import ColorList from "./ColorList";
 // jest.mock("../api/fetchColors");
 
 test("Fetches data and renders the bubbles", 
-// async 
-() => {
+async () => {
   // Finish this test
 
   // mockFetchColors.mockResolvedValueOnce(colors);
@@ -38,16 +37,15 @@ test("Fetches data and renders the bubbles",
   // expect(findAllByTestId(/color/i)).toHaveLength(10)
 
   const fetchedColorList = findAllByTestId(/color/i)
-  // console.log("Fetched Color List: ", fetchedColorList)
+  console.log("Fetched Color List: ", fetchedColorList)
 
   //Assert 
-  // await waitFor(() => {
-  //   expect(getAllByTestId(/color/i)).toHaveLength(10)
-  // })
+  await waitFor(() => {
+    expect(getAllByTestId(/color/i)).toHaveLength(3)
+  })
 
 });
 
 const colors = 
 
-[{"color":"alicebluez","code":{"hex":"#f0f8ff"},"id":1},{"color":"limegreen","code":{"hex":"#99ddbc"},"id":2},{"color":"aqua","code":{"hex":"#00ffff"},"id":3},{"color":"aquamarine","code":{"hex":"#7fffd4"},"id":4},{"color":"lilac","code":{"hex":"#9a99dd"},"id":5},{"color":"softpink","code":{"hex":"#dd99ba"},"id":6},{"color":"softyellow","code":{"hex":"#dcdd99"},"id":8},{"color":"blue","code":{"hex":"#6093ca"},"id":10},{"color":"blueviolet","code":{"hex":"#8a2be2"},"id":11}]
-
+[{"color":"alicebluez","code":{"hex":"#f0f8ff"},"id":1},{"color":"limegreen","code":{"hex":"#99ddbc"},"id":2},{"color":"aqua","code":{"hex":"#00ffff"},"id":3}]
