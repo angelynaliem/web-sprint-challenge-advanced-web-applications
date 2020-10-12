3bbb34280378414c247770949da9118a5b785210
angelynaliem <angelynaliem@gmail.com>
2020-10-11 22:08:33 -0400

    Login page built

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,88 +1,74 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { useHistory } from "react-router-dom";
 
-// import { axiosWithAuth } from ""
+import { axiosWithAuth } from "../util/axiosWithAuth";
 
 const initialLoginForm = {
 
-    
+  username: "",
+  password: ""
+
 }
 
 const Login = () => {
 
-  const 
+  const { push } = useHistory();
+
+  const [login, setLogin] = useState(initialLoginForm);
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  // useEffect(() => {
+  //   submitLogin()
+  // }, [])
+
+  const submitLogin = (e) => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post("/login", login)
+    .then((res) => {
+        console.log("Login Res is: ", res)
+        localStorage.setItem("token", res.data.payload)
+        // push("/protected")
+    })
+    .catch((err) => {
+      console.log("Login Err is: ", err)
+    })
+  }
+
+  const handleChangeLogin = (e) => {
+    e.persist()
+    setLogin({
+      ...login,
+      [e.target.name] : e.target.value
+    })
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <div>
+        <form onSubmit={submitLogin}>
+           <input
+            type="text"
+            name="username"
+            value={login.username}
+            onChange={handleChangeLogin}
+            placeholder="username"
+          />
+          <input
+            type="password"
+            name="password"
+            value={login.password}
+            onChange={handleChangeLogin}
+            placeholder="password"
+          />
+          <button>Log in</button>
+        </form>
+      </div>
     </>
   );
 };
 
 export default Login;
-
-
-// // Step 2: set up login (doesnt need authentication yet, just that user can log in)
-
-// import React from "react";
-// import axios from "axios";
-
-// import { axiosWithAuth } from "../util/axiosWithAuth";
-
-// class Login extends React.Component {
-//   state = {
-//     credentials: {
-//       username: "",
-//       password: ""
-//     }
-//   };
-
-//   handleChange = (e) => {
-//     this.setState({
-//       credentials: {
-//         ...this.state.credentials,
-//         [e.target.name]: e.target.value
-//       }
-//     });
-//   };
-
-//   login = (e) => {
-//     e.preventDefault();
-//     axiosWithAuth()
-//       .post("/login", this.state.credentials)
-//       .then((res) => {
-//         localStorage.setItem("token", res.data.payload);
-//         this.props.history.push("/protected");
-//       })
-//       .catch((err) => {
-//         console.log("Err is: ", err);
-//       });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <form onSubmit={this.login}>
-//           <input
-//             type="text"
-//             name="username"
-//             value={this.state.credentials.username}
-//             onChange={this.handleChange}
-//           />
-//           <input
-//             type="password"
-//             name="password"
-//             value={this.state.credentials.password}
-//             onChange={this.handleChange}
-//           />
-//           <button>Log in</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Login;
 
